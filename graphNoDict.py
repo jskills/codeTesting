968921d1924fe3ceea3335c2cfdbcda8f43f9df8
Jim Mortko <jmortko@hearst.com>
2020-03-02 16:18:55 -0500

    rewrote it

--- graphNoDict.py
+++ graphNoDict.py
@@ -1,85 +1,50 @@
 
-class Graph:
-    def __init__(self, data, left=None, right=None, isRoot=True):
-        self.left = left
-        self.right = right
+class GraphNode:
+    def __init__(self, data):
         self.data = data
-        self.isRoot = isRoot
+        self.vertexList = list()
 
-    def addVertex(self, rightVal=None, leftVal=None):
-        if rightVal:
-            node = Graph(rightVal)
-            self.right = node
-        if leftVal:
-            node = Graph(leftVal)
-            self.left = node
-        return node 
+    def addVertex(self, vertexVal):
+        if vertexVal not in self.vertexList:
+            newNode = GraphNode(vertexVal)
+            self.vertexList.append(newNode)
+        return newNode
 
-    def connectVertex(self, rightNode=None, leftNode=None):
-        if rightNode:
-            self.right = rightNode
-        if leftNode:
-            self.left = leftNode
-            
+    def connectVertex(self, node, vertexNode):
+        node.vertexList.append(vertexNode)
+        
 
-    def depthFirst(self):
-	# pre-order
-        print(self.data)
-        if self.left:
-           self.left.depthFirst()
-        if self.right:
-            self.right.depthFirst()
-
-
-    # Function to  print level order traversal of tree 
-    def printLevelOrder(self, root): 
-        h = self.height(root) 
-        for i in range(1, h+1): 
-            self.printGivenLevel(root, i) 
-  
-  
-    # Print nodes at a given level 
-    def printGivenLevel(self, root , level): 
-        if root is None: 
+    def printGraph(self, node, visited=list()):
+        # starting with one node, print all connections in a directed graph
+        if node.data:
+            if len(node.vertexList):
+                for v in node.vertexList:
+                    print(node.data + "->" + v.data)
+                    if v.data not in visited:
+                        visited.append(v.data)
+                        self.printGraph(v, visited)
+                else:   
+                    return
+        else:
             return
-        if level == 1: 
-            print str(root.data), 
-        elif level > 1 : 
-            self.printGivenLevel(root.left , level-1) 
-            self.printGivenLevel(root.right , level-1) 
-  
-  
-    def height(self,node): 
-        if node is None: 
-            return 0 
-        else : 
-            # Compute the height of each subtree  
-            lheight = self.height(node.left) 
-            rheight = self.height(node.right) 
-  
-            #Use the larger one 
-            if lheight > rheight : 
-                return lheight+1
-            else: 
-                return rheight+1
 
-###################################
+    
 
-root = Graph('A')
-nodeB = root.addVertex(rightVal='B')
-nodeC = root.addVertex(leftVal='C')
-nodeD = nodeC.addVertex(rightVal='D')
-nodeE = nodeD.addVertex(rightVal='E')
-nodeJ = nodeD.addVertex(leftVal='J')
+###############################################
+
+a = GraphNode('A')
+b = a.addVertex('B')
+c = a.addVertex('C')
+b.connectVertex(b,c)
+d = b.addVertex('D')
+e = GraphNode('E')
+f = c.addVertex('F')
+g = f.addVertex('G')
+c.connectVertex(c,d)
+d.connectVertex(d,c)
+g.connectVertex(g,d)
+e.connectVertex(e,f)
 
 
+b.printGraph(a)
 
-print "Depth First"
-root.depthFirst()
-
-print "Breadth First"
-root.printLevelOrder(root) 
-
-print "\nHeight of Graph"
-print str(root.height(root))
-print str(root.height(nodeD))
