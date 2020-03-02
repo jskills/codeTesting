4be1c19e88c303e10e7383ab1d17c52b4e75a878
JSkills <jskills@jskills.com>
2020-03-01 22:40:37 -0500

    added comments

--- graph.py
+++ graph.py
@@ -6,22 +6,35 @@ class Graph():
         self.graphDict = dict()
 
     def find_all_paths(self, graph, start, end, path=[]):
-	# create a new list. Using path.append(start) would modify the value of path in the caller
+	# create a new list.
+        # path = path + [start] is necessary since
+        # Using path.append(start) would modify the value of path in the caller
         path = path + [start]
         if start == end:
+            # of start is the same as end return [path] and end recursion
             return [path]
         if not start in graph:
+            # if start param does not even exist in the graph return empty list
             return []
         all_paths = []
+        # since our graphDict is in the form of : nodename, list of all nodes it is connected to
+        # iterate over each unique nodename and pass each of the nodes it's connected to back into this method
         for node in graph[start]:
+            # the path array gets passed back into this method recursively
+            # if the value of the connected node is not in our path array
+            # recursively call this method passing in the connected node as the start node with the same end
             if node not in path:
                 newpaths = self.find_all_paths(graph, node, end, path)
+                # since we will get multiple paths from this call,
+                # iterate and append them to the all_paths array which is returned at the end of this method
                 for newpath in newpaths:
                     all_paths.append(newpath)
         return all_paths
 
 
     def find_shortest_rpath(self, graph, start, end, path=[]):
+        # finding the shortest path is almost exactly the same as finding all paths
+        # except instead of augmenting a long list of paths, we just store the one with the shortest length
         path = path + [start]
         if start == end:
             return path
@@ -62,6 +75,7 @@ class Graph():
 
 
     def BFSearch(self, node):
+        # breadth first search from any node in the graph
         # use different Dict structure (one entry per edge)
         newDict = {}
         newDict = self.generateEdges(self.graphDict)
