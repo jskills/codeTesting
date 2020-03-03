bb3c6ad519cd4d15d174ac469f31496b1bd1f86a
Jim Mortko <jmortko@hearst.com>
2020-03-02 21:16:24 -0500

    n/a

--- recursion.py
+++ recursion.py
@@ -11,6 +11,21 @@ def getStringPermutations(s, step=0):
         # recurse on the portion of the string that has not been swapped yet
         getStringPermutations(s_copy, step + 1)
 
+def retStringPermutations(s, step=0, allP=list()):
+    if step == len(s):
+        # we've gotten to the end, add permutation to return list
+        print("".join(s))
+        allP.append("".join(s))
+    for i in range(step, len(s)):
+        # copy the string (store as array)
+        s_copy = list(s)
+        # swap the current index with the step
+        s_copy[step], s_copy[i] = s_copy[i], s_copy[step]
+        # recurse on the portion of the string that has not been swapped yet
+        retStringPermutations(s_copy, step + 1, allP)
+
+        return allP
+
 
 def isPalindrome(s1, s2):
     if foo:
@@ -18,5 +33,6 @@ def isPalindrome(s1, s2):
 
 #############
 
-print(getStringPermutations('WOCK'))
+#getStringPermutations('WOCK')
+print(retStringPermutations('WOCK'))
 
